df111781e4710b664688e3a6d6a4375c35e79d4c
ovidiufilip10 <ovidiufilip10@yahoo.com>
2023-02-27 17:26:00 +0200

    Completed Book Library

--- script.js
+++ script.js
@@ -20,34 +20,72 @@ const listaCompleta = [
       numarCititori: 28,
     },
     {
-      titlu: "Information Theory",
-      autor: "Shannon",
-      numarCititori: 12,
+      titlu: "The Witcher",
+      autor: "Andrzej Sapkowski",
+      numarCititori: 54,
+    },
+    {
+        titlu: "Information Theory",
+        autor: "Shannon",
+        numarCititori: 12,
     }
   ];
+  const removeCurentDisplay = (element) =>{
+    while (element.hasChildNodes()) {
+      element.removeChild(element.firstChild);
+    }
+}
+
+  function displayFilteredList(listaFiltrata){
+    listaFiltrata.forEach((book) => {
+    const row = document.createElement('tr');
+  
+    const celulaTitlu = document.createElement('td');
+    celulaTitlu.innerText = book.titlu;
+    row.appendChild(celulaTitlu);
+  
+    const celulaAutor = document.createElement('td');
+    celulaAutor.innerText = book.autor;
+    row.appendChild(celulaAutor);
+  
+    const celulaNumarCitiori = document.createElement('td');
+    celulaNumarCitiori.innerText = `${book.numarCititori}`;
+    row.appendChild(celulaNumarCitiori);
+  
+    bodyTabelCarti.appendChild(row);
+  });
+  }
   
   const listaFiltrata = [...listaCompleta];
+  const bodyTabelCarti = document.querySelector("#bodyTabelCarti");
+  displayFilteredList(listaFiltrata);
   const searchButton = document.querySelector("#searchButton");
   const resetButton = document.querySelector("#resetButton");
+  const searchText = document.querySelector("#searchText");
+  const errorText = document.querySelector(".errorText");
+  const resultText = document.querySelector(".resultText")
 
-
-
-  const bodyTabelCarti = document.querySelector("#bodyTabelCarti");
-
-  listaFiltrata.forEach((book) => {
-  const row = document.createElement('tr');
-
-  const celulaTitlu = document.createElement('td');
-  celulaTitlu.innerText = book.titlu;
-  row.appendChild(celulaTitlu);
-
-  const celulaAutor = document.createElement('td');
-  celulaAutor.innerText = book.autor;
-  row.appendChild(celulaAutor);
-
-  const celulaNumarCitiori = document.createElement('td');
-  celulaNumarCitiori.innerText = `${book.numarCititori}`;
-  row.appendChild(celulaNumarCitiori);
-
-  bodyTabelCarti.appendChild(row);
-});
+    resetButton.addEventListener("click", function(){
+        searchText.value = "";
+        resetButton.style.display = "none";
+        resultText.style.display = "none";
+        removeCurentDisplay(bodyTabelCarti);
+        console.log(listaFiltrata);
+        displayFilteredList(listaFiltrata);
+    }
+    );
+    searchButton.addEventListener("click", function(){
+        if(searchText.value.length < 3 ){
+            errorText.style.display = "flex";
+            return ;
+        }
+        removeCurentDisplay(bodyTabelCarti);
+        errorText.style.display = "none";
+        resetButton.style.display = "flex";
+        const newList = listaFiltrata.filter( book => { return book.titlu.toLowerCase().includes(searchText.value.toLowerCase())===true || book.autor.toLowerCase().includes(searchText.value.toLowerCase())===true 
+        })
+        displayFilteredList(newList);
+        resultText.innerHTML=`${newList.length} Rezultate`;
+        resultText.style.display = "flex";
+    }
+    );
